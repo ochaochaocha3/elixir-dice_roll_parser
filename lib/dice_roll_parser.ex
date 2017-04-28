f6f8fb18ad0c51839a62a6361e31570a4570f959
ocha <ochaochaocha3@gmail.com>
2017-04-29 01:48:12 +0900

    mix new

---
+++ lib/dice_roll_parser.ex
@@ -0,0 +1,18 @@
+defmodule DiceRollParser do
+  @moduledoc """
+  Documentation for DiceRollParser.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DiceRollParser.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/dice_roll_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule DiceRollParserTest do
+  use ExUnit.Case
+  doctest DiceRollParser
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
